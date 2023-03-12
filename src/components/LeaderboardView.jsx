4fc4b1ccf270e9286a69152131cc5ecb3d87c4b8
Gareth Ng <ngys.gareth@gmail.com>
2023-03-12 07:50:34 -0700

    commited changes

--- src/components/LeaderboardView.jsx
+++ src/components/LeaderboardView.jsx
@@ -64,89 +64,53 @@ function LeaderboardView() {
 
 
   return (
-    <div className='h-screen bg-beige flex flex-col text-black'>
+    <div className="h-screen text-black">
       {/* Top King of Applications and Rejections */}
-      <div className='h-1/3 flex flex-col my-3'>
-        <div className='flex flex-col items-center justify-center'>
-
-          <img className="mx-auto my-auto" src={Leadboards[0].pic} alt={Leadboards[0].name}></img>
-          <p className='text-lg font-saira text-navy'>{Leadboards[0].name}</p>
-
-
-          {/* Deets */}
-          <div className='flex gap-3 items-center'>
-            {/* Applications */}
-
-            <p className='text-md flex'>
-              <object className="mx-auto my-auto" type="image/svg+xml" data={application}></object>
-              <p className='m-auto'>{Leadboards[0].applications}</p>
-            </p>
-
-
-            {/* Rejection */}
-            <p className='text-m flex'>
-
-              <object className="mx-auto my-auto" type="image/svg+xml" data={rejection}></object>
-
-              <p className='m-auto'>{Leadboards[0].rejections}</p>
-            </p>
+      <div className="flex flex-col items-center justify-center mt-10">
+        <img className="w-24 h-24 object-cover rounded-full" src={Leadboards[0].pic} alt={Leadboards[0].name}></img>
+        <p className="text-xl font-semibold mt-2">{Leadboards[0].name}</p>
+        <div className="flex gap-2 mt-2">
+          <div className="flex items-center">
+            <img className="w-6 h-6 object-contain" src={application} alt=""></img>
+            <p className="ml-1">{Leadboards[0].applications}</p>
+          </div>
+          <div className="flex items-center">
+            <img className="w-6 h-6 object-contain" src={rejection} alt=""></img>
+            <p className="ml-1">{Leadboards[0].rejections}</p>
           </div>
         </div>
       </div>
-
+  
       {/* Rankings */}
-      <p className="text-4xl m-3 font-bold font-saira text-dark-navy text-center mt-10">RANKINGS:</p>
-
+      <p className="text-4xl font-bold mt-10 text-center">Rankings</p>
+  
       {/* People Ranking List */}
-
-      <div className="flex flex-col gap-1 overflow-scroll">
+      <div className="flex flex-col gap-4 mt-8 px-4">
         {/* Each card */}
         {sortedLeaderBoards.map(({ id, link, pic, name, rejections, applications, datetime, comment }, index) => {
           return (
-            <div className="p-2 m-1 flex justify-between border-solid bg-navy rounded-md mx-2" key={id}>
-
-
-              <div className='flex gap-2 w-1/5'>
-                <p className='text-navy m-auto font-saira'>{index + 1}</p>
-                <div className='flex flex-col'>
-                <img src={pic} alt="" className='grow' />
-                <p className="font-saira text-center text-white">{name}</p>
-
-
-                </div>
-                
+            <div className="bg-dark-gray-900 rounded-md flex gap-4 p-4" key={id}>
+              <div className="w-1/5 flex flex-col justify-center items-center">
+                <p className="text-xl font-bold">{index + 1}</p>
+                <img className="w-20 h-20 object-cover rounded-full mt-2" src={pic} alt=""></img>
+                <p className="text-center mt-2">{name}</p>
               </div>
-
-
-              {/* Information */}
-              <div className="flex flex-col grow">
-                <div className='information grow justify-evenly flex w-4/5 m-auto'>
-                  <div className="flex">
-                    <object className="mx-auto my-auto" type="image/svg+xml" data={application}></object>
-
-                    <p className='m-auto'>{applications}</p>
-
+              <div className="flex-grow flex items-center justify-between">
+                <div className="flex gap-2">
+                  <div className="flex items-center">
+                    <img className="w-6 h-6 object-contain" src={application} alt=""></img>
+                    <p className="ml-1">{applications}</p>
                   </div>
-
-                  <div className='flex'>
-                    <object className="mx-auto my-auto" type="image/svg+xml" data={rejection}></object>
-                    <p className='m-auto font-saira'>{rejections}</p>
-
-
+                  <div className="flex items-center">
+                    <img className="w-6 h-6 object-contain" src={rejection} alt=""></img>
+                    <p className="ml-1">{rejections}</p>
                   </div>
                 </div>
-
               </div>
-
-
             </div>
-
           )
         })}
-
       </div>
-
-
     </div>
   )
 }
